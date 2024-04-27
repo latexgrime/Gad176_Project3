94cc2ac1fd8daaffa9df3cf3ac90bdd203176a3b
Leonardo Estigarribia <leonardo.estigarribia@student.saedubai.com>
2024-04-27 12:49:47 +0400

    Added Tariq scripts into the project and implemented the Main Menu screen

---
+++ Assets/Tariq Ali Lafta/Tariq - Inventory/InventoryManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TariqAliLafta
+{
+    public class InventoryManager : MonoBehaviour
+    {
+
+        public GameObject inventorymenu;
+        private bool menuactivated;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E) && menuactivated) 
+            {
+                Time.timeScale = 1;
+                inventorymenu.SetActive(false);
+                menuactivated = false;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.E) && !menuactivated)
+            {
+                Time.timeScale = 0;
+                inventorymenu.SetActive(true);
+                menuactivated = true;
+            }
+
+        }
+
+        public void AddItem(string ItemName, int quantity, Sprite itemsprite) 
+        {
+            Debug.Log ("itemName" +  ItemName + "quantity =" + quantity + "itemSprite" + itemsprite);
+        }
+    }
+}
