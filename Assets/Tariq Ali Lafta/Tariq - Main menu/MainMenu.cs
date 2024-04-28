616e904c5cff97e7235fcea168f47fa51491d4e5
TariqStudent <159271240+TariqStudent@users.noreply.github.com>
2024-04-28 16:13:02 +0400

    Created a main menu, and added it to the build setting accordingly.

--- Assets/Tariq Ali Lafta/Tariq - Main menu/MainMenu.cs
+++ Assets/Tariq Ali Lafta/Tariq - Main menu/MainMenu.cs
@@ -7,6 +7,7 @@ namespace TariqAliLafta
 {
     public class MainMenu : MonoBehaviour
     {
+    //main menua should be first (0), and develop scene should be next (1).
         public void PlayGame()
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
